36e305397af0c342e53b88dd3c0cfe410906390b
DuyManh122 <nguyendangduymanh2003@gmail.com>
2023-12-02 08:31:04 +0700

    add calculating functions

---
+++ regression/Regression_nonlinear_expx.m
@@ -0,0 +1,19 @@
+function [f,r2] = regression_nonlinear_expx(x_num,y_num)     
+    x_data = x_num;
+    y_data = log(y_num);
+    n = length(x_data);
+    sumx = sum(x_data);
+    sumy = sum(y_data);
+    sumxy = sum(x_data.*y_data);
+    sumx2 = sum(x_data.^2);
+    x_tb = sumx / n;
+    y_tb = sumy / n;
+    a1 = (n*sumxy - sumx*sumy)/(n*sumx2 - sumx*sumx);
+    a0 = y_tb - a1*x_tb;
+    beta = a1;
+    alpha = exp(a0);
+    f = @(x) (alpha.*exp(x*beta));                    
+    Sr = sum((y_data-a0-a1*x_data).^2);
+    St = sum((y_data-y_tb).^2);
+    r2 = (St - Sr) / St;
+end
